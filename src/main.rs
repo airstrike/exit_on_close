438a94d67e63fa41be4f89aeace3e528ffa31e12
Andy Terra <152812+airstrike@users.noreply.github.com>
2024-07-10 14:07:30 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use iced::{
+    self,
+    widget::{container, text},
+    Element, Length, Task, Theme,
+};
+
+pub fn main() -> iced::Result {
+    iced::application("MyApp", MyApp::update, MyApp::view)
+        .theme(MyApp::theme)
+        .settings(iced::Settings::default())
+        .run()
+}
+
+#[derive(Default)]
+pub struct MyApp {
+    theme: Theme,
+}
+
+#[derive(Debug)]
+pub enum Message {}
+
+impl MyApp {
+    pub fn theme(&self) -> Theme {
+        self.theme.clone()
+    }
+
+    pub fn update(&mut self, _message: Message) -> Task<Message> {
+        Task::none()
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        container(text("Hello from iced!"))
+            .center(Length::Fill)
+            .into()
+    }
+}
